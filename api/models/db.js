f2d42cb49d2097d73d5bf54f43a83c5fbd51f7b5
Dyrits <dyrits@outlook.com>
2022-05-05 17:25:15 -0500

    PRODUCTION > production

--- api/models/db.js
+++ api/models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let uri = 'mongodb://localhost/country_data';
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
+if (process.env.NODE_ENV === 'production') {
     uri = process.env.MONGODB_URI;
 }
 
