d80ecb34733025bca0888e1b8936f43e17b9eaba
Dyrits <dyrits@outlook.com>
2022-05-05 17:14:19 -0500

    Setting up to set app online.

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.use((req, res, next) => {
     res.$json = (status, data) => { res.status(status).json({...data, status}); }
     next();
